95c544337ce9e72a19a9464252a0dbf5005d2975
joaobrandao <joaobrandao28.work@gmail.com>
2023-12-06 11:21:45 -0300

    atualização final

--- Proj site/js/main.js
+++ Proj site/js/main.js
@@ -1,4 +1,6 @@
 var RotasApp = angular.module("RotasApp",["ngRoute"])
+var Dialog = true
+var menu = false
 
     RotasApp.config(function($routeProvider){
         $routeProvider
@@ -16,28 +18,38 @@ var RotasApp = angular.module("RotasApp",["ngRoute"])
         })
         .when("/sobre", {
             templateUrl:"./templates/sobre.html",
-            controller: "sobre"
+            controller: "lugares"
         })
     });
 
 RotasApp.controller("home",function($scope,$http,$route){
+        menu = false
         let Menu = document.getElementById("menu_aux")
         //console.log(Menu)
         if(Menu != null)
         {
             Menu.remove()
         }
-        
+        if(Dialog == false){
+            document.getElementsByClassName("mini_screen")[0].remove()
+        }
         $scope.CloseFunc = function(){
             document.getElementsByClassName("mini_screen")[0].remove()
-            document.getElementsByClassName("Black_screen")[0].remove()
+            Dialog = false
             }
 
 });
     
+
+
 RotasApp.controller("lugares",function($scope,$http,$route){
     $scope.ArtigoFinal = []
-    document.getElementById("Topo").innerHTML+="<div id='menu_aux'><a href='#/'><h1>Home</h1></a><a href='#!/lugares'><h1>Lugares</h1></a><a href='#!/sobre'><h1>Sobre</h1></a></div>"
+    
+    if(menu == false){
+        document.getElementById("Topo").innerHTML+="<div id='menu_aux'><a href='#/'><h1>Home</h1></a><a href='#!/lugares'><h1>Lugares</h1></a><a href='#!/experiencias'><h1>Experiencias</h1></a><a href='#!/sobre'><h1>Sobre</h1></a></div>"
+    }
+    
+    menu = true
     var ArtigoFinal = $http.get("./dados/Livros.json")
     .then(function(respone){
         // console.log(respone)
@@ -48,23 +60,12 @@ RotasApp.controller("lugares",function($scope,$http,$route){
     })
 })
 
-// RotasApp.controller("sobre",function($scope,$http,$route){
-//     $scope.ArtigoFinal = []
-//     document.getElementById("Topo").innerHTML+="<div id='menu_aux'><a href='#/'><h1>Home</h1></a><a href='#!/lugares'><h1>Lugares</h1></a><a href='#!/sobre'><h1>Sobre</h1></a></div>"
-//     var ArtigoFinal = $http.get("./dados/Livros.json")
-//     .then(function(respone){
-//         // console.log(respone)
-//         respone.data.forEach(Livros =>{
-//             console.log(Livros)
-//             $scope.ArtigoFinal.push(Livros)
-//         })
-//     })
-// })
-
 RotasApp.controller("experiencias",function($scope,$http,$route){
     $scope.nome = "artigos"
+
     $scope.PromFunc = async function(){
-        const Res = await fetch('../dados/artigos.json')
+        console.log("O")
+        fetch('./dados/artigos.json')
         .then(response => response.json())
         .then(json => {
           $scope.Artigos = json
@@ -72,10 +73,17 @@ RotasApp.controller("experiencias",function($scope,$http,$route){
           document.getElementById("art1").innerHTML+=`
           <div class='Artigo_sq'>
             <h1>`+$scope.Artigos[index]["Titulo"]+`</h1>
-            <h3>`+$scope.Artigos[index]["Autor"]+`</h3>
+            <h4>`+$scope.Artigos[index]["Autor"]+`</h4>
+            <h3>`+$scope.Artigos[index]["Conteudo"]+`</h3>
           </div> `
          }})
       }
 
     $scope.PromFunc()
-})
+
+    $scope.ArtigoFinal = []
+    if (menu == false){
+        document.getElementById("Topo").innerHTML+="<div id='menu_aux'><a href='#/'><h1>Home</h1></a><a href='#!/lugares'><h1>Lugares</h1></a><a href='#!/experiencias'><h1>Experiencias</h1></a><a href='#!/sobre'><h1>Sobre</h1></a></div>"
+    }
+    menu = true
+    })
